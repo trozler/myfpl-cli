62498388ae00cbe338fe533b5bd1de19d9fb2977
Tony Rosler <tony.rosler246@gmail.com>
2019-08-24 00:27:05 +0200

    Complete update of repository making script compatible with 2019/2020 fpl_api end-points. Including new authentication requirements

--- README.md
+++ README.md
@@ -1,13 +1,64 @@
-# fpl_checker
-Simple python script that takes advanatge of the fpl api. Allows users to retrieve information about their fantasy premier league teams.
-Script can retrieve:
-- Total Pointes for the gameweek and points per player
-- Changes in prices for a player
-- Changes in standings in leagues
-- Player news
+## Fantasy Premier League CLI team checker
+A python script designed for command line. Allows fpl users to skip the laborious web log in flow in a single command. 
 
-And many other features. 
 
-Users are required to know there fpl id number:
-Id can be retreived by going to "my team", click "view gameweek history" and looking at url.
+
+NOTE/
+Users are required to know their **unique fpl entry id**. The id can be retreived on the fpl site by clicking "my team", "view gameweek history" and looking at url.
+Example: https://fantasy.premierleague.com/entry/663372/history
+
+
+SAMPLE OUTPUT:
+
+                                    Points           +/-    Chance
+Name                                (GW)     Price   (GW)   NextGW   News
+
+Patrício                            0        5.0     0.0    100      
+Robertson                           0        7.0     0.0    100      
+van Dijk                            0        6.5     0.0    100      
+Zinchenko                           0        5.5     0.0    100      
+Wan-Bissaka                         0        5.6     0.0    100      
+Sterling                  (C)       0        12.2    0.0    100      
+De Bruyne                           0        9.6     0.0    100      
+Pérez                               0        6.4     0.0    100      
+Mané                      (VC)      0        11.5    0.0    100      
+Jota                                0        6.5     0.0    100      
+Barnes                              0        6.6     0.0    100      
+Button                    (Bench)   0        4.0     0.0    100      
+Dendoncker                (Bench)   0        4.5     0.0    100      
+Kelly                     (Bench)   0        4.0     0.0    100      
+Greenwood                 (Bench)   0        4.5     0.0    100      
+
+
+Gameweek points: 0              Overall points: 133    
+Gameweek rank:   None           Overall rank:   478097 
+
+
+H2H Leagues:
+                                        Prev       Curr       Rank        Leader
+League Name                             Rank       Rank       Diff
+
+Rob Hughes got banged II                5          5          0           6 (0)
+
+
+Classic Leagues:
+                                        Prev       Curr       Rank        Leader
+League Name                             Rank       Rank       Diff        Points
+
+Man City                                19232      19232      0           189 (56)
+England                                 170785     170785     0           202 (69)
+Gameweek 1                              478097     478097     0           205 (72)
+Overall                                 478097     478097     0           205 (72)
+@OfficialFPL on Twitter                 8606       8606       0           205 (72)
+Rob Hughes Got Banged                   15         15         0           171 (38)
+@FPLEditor's Charity League             2281       2281       0           201 (68)
+
+
+
+
+
+
+
+
+
 
--- gameweek_check_working.py
+++ gameweek_check_working.py
@@ -1,26 +1,14 @@
-import requests
-import traceback
-import re
+import requests, traceback, re
 
-
-#FPL ID
-#663372
-
-#Overall fpl api
-#https://fantasy.premierleague.com/api/entry/663372/
-#Team api
-#https://fantasy.premierleague.com/api/my-team/663372/
-
-
-#Set password and login (login is equivelent to email) fileds for easy future use 
-credentials = {'password':'Drogba1997',
-               'login':'tony.rosler246@gmail.com',
+#Hardcode password and login (login is equivelent to email) fields for future 
+credentials = {'password':None,
+               'login':None,
                'redirect_uri': 'https://fantasy.premierleague.com/a/login',
                'app': 'plfpl-web'
                }
 
 #Set team_id for future use by changing None
-team_id = 663372
+team_id = None
 
 #Only ask for validation if team_id, email and password havn't been set
 if team_id == None or credentials['password'] == None or credentials['password'] == None:
@@ -47,133 +35,113 @@ if team_id == None or credentials['password'] == None or credentials['password']
 
         break
 
-
+#Bootstrap API
 bootstrap_url = 'https://fantasy.premierleague.com/api/bootstrap-static/'
 get_data_bootstrap = requests.get(bootstrap_url).json()
-
-#gweek_url = 'https://fantasy.premierleague.com/api/entry/%s/' % (team_id)
-#gweek = requests.get(gweek_url).json()
-#gameweek = gweek['current_event']
 
 #Establish session
 session = requests.session()
 session.post('https://users.premierleague.com/accounts/login/', data=credentials)
 
-#Get team information using session object
+#Team API
 team_api = 'https://fantasy.premierleague.com/api/my-team/%s/' % (team_id)
 get_data_team = session.get(team_api).json()
 
+#Rank section
+entry_api = 'https://fantasy.premierleague.com/api/entry/%s/' % team_id
+get_data_entry = session.get(entry_api).json()
 
-team = []
-status = []
-
-
-#Querry transfers and see if zero. If not zero then replace teh transferd in player with teh old player/ 
-
+#Transfer API
 transfer_api = 'https://fantasy.premierleague.com/api/entry/%s/transfers-latest/' % (team_id)
 get_transfer = session.get(transfer_api).json()
 
-
-#If length is equal to 0 then 
-if (len(get_transfer) == 0):
-    
-
-
-
-
-
-
-
-for i in range(len(get_data_team['picks'])):
-    player_id = get_data_team['picks'][i]['element']
-    multiplier = get_data_team['picks'][i]['multiplier']
-    vice_captain = get_data_team['picks'][i]['is_vice_captain']
-    position = get_data_team['picks'][i]['position']
-
-    if multiplier == 2:
-        player_status = "(C)"
-    elif multiplier == 3:
-        player_status = "(TC)"
-    elif vice_captain == True:
-        player_status = "(VC)"
-    elif position > 11:
-        player_status = "(Bench)"
-    else:
-        player_status = ""
-
-    team.append(player_id)
-    status.append(player_status)
-
+team = []
+status = []
 sp = ' '
-print ("\n" + sp*36 + "Points" + sp*11 + "+/-" + sp*4 + "Chance")
-print ("Name" + sp*32 + "(GW)" + sp*5 + "Price" + sp*3 + "(GW)" + sp*3 + "NextGW" + sp*3 + "News\n")
 
-for i in range(len(team)):
-    id = team[i]
-    player_status = status[i]
-    for i in range(len(get_data_bootstrap['elements'])):
-        if get_data_bootstrap['elements'][i]['id'] == id:
-            name = get_data_bootstrap['elements'][i]['web_name']
-            gw_points = get_data_bootstrap['elements'][i]['event_points']
-            if player_status == "(C)":
-                gw_points *= 2
-            elif player_status == "(TC)":
-                gw_points *= 3
-            price = get_data_bootstrap['elements'][i]['now_cost'] / 10
-            price_change = get_data_bootstrap['elements'][i]['cost_change_event'] / 10
-            next_round = get_data_bootstrap['elements'][i]['chance_of_playing_next_round']
-            if next_round == None:
-                next_round = 100
-            news = get_data_bootstrap['elements'][i]['news']
+def get_player_info(transfers:bool):
+    
+    for i in range(len(get_data_team['picks'])):
+        player_id = get_data_team['picks'][i]['element']
+        multiplier = get_data_team['picks'][i]['multiplier']
+        vice_captain = get_data_team['picks'][i]['is_vice_captain']
+        position = get_data_team['picks'][i]['position']
 
-            print("%-25s %-9s %-8d %-7.1f %-6.1f %-8d %s" %
-                (name, player_status, gw_points, price, price_change, next_round, news))
+        if multiplier == 2:
+            player_status = "(C)"
+        elif multiplier == 3:
+            player_status = "(TC)"
+        elif vice_captain == True:
+            player_status = "(VC)"
+        elif position > 11:
+            player_status = "(Bench)"
+        else:
+            player_status = ""
+
+        team.append(player_id)
+        status.append(player_status)
+
+    if transfers:
+        #replace transferd in players with transferd out
+        for i in range(len(get_transfer)):
+            player_out = get_transfer[i]['element_out']
+            player_in = get_transfer[i]['element_in']
+            team[team.index(player_in)] = player_out
+        
+    print ("\n" + sp*36 + "Points" + sp*11 + "+/-" + sp*4 + "Chance")
+    print ("Name" + sp*32 + "(GW)" + sp*5 + "Price" + sp*3 + "(GW)" + sp*3 + "NextGW" + sp*3 + "News\n")
+
+    for i in range(len(team)):
+        id = team[i]
+        player_status = status[i]
+        for i in range(len(get_data_bootstrap['elements'])):
+            if get_data_bootstrap['elements'][i]['id'] == id:
+                name = get_data_bootstrap['elements'][i]['web_name']
+                gw_points = get_data_bootstrap['elements'][i]['event_points']
+                if player_status == "(C)":
+                    gw_points *= 2
+                elif player_status == "(TC)":
+                    gw_points *= 3
+                price = get_data_bootstrap['elements'][i]['now_cost'] / 10
+                price_change = get_data_bootstrap['elements'][i]['cost_change_event'] / 10
+                next_round = get_data_bootstrap['elements'][i]['chance_of_playing_next_round']
+                if next_round == None:
+                    next_round = 100
+                news = get_data_bootstrap['elements'][i]['news']
+
+                print("%-25s %-9s %-8d %-7.1f %-6.1f %-8d %s" %
+                    (name, player_status, gw_points, price, price_change, next_round, news))
+
+#If length is equal to 0 then no transfers we just enter regular flow
+if (len(get_transfer) == 0):
+    get_player_info(False)
+else:
+    get_player_info(True)
 
 print ("\n\nGameweek points: %-7d\tOverall points: %-7d" %
-    (get_data_team['entry_history']['points'], get_data_team['entry_history']['total_points']))
+    (get_data_entry["summary_event_points"], get_data_entry["summary_overall_points"]))
 
-print ("Gameweek rank:   %-7d\tOverall rank:   %-7d\n" %
-    (get_data_team['entry_history']['rank'], get_data_team['entry_history']['overall_rank']))
-
-
-#Rank section
-#https://fantasy.premierleague.com/api/entry/663372
-
-entry_api = 'https://fantasy.premierleague.com/api/entry/%s' % team_id
-get_data_entry = get_data_team = requests.get(entry_api).json()
-
-#print(get_data_entry)
+print ("Gameweek rank:   %-7s\tOverall rank:   %-7s\n" %
+    (get_data_entry["summary_event_rank"], get_data_entry["summary_overall_rank"]))
 
 print ("\nH2H Leagues:\n")
 print (sp*40 + "Prev" + sp*7 + "Curr" + sp*7 + "Rank" + sp*8 + "Leader")
-print ("League Name" + sp*29 + "Rank" + sp*7 + "Rank" + sp*7 + "Diff" + sp*8 + "Points\n")
+print ("League Name" + sp*29 + "Rank" + sp*7 + "Rank" + sp*7 + "Diff\n")
 
 for i in range(len(get_data_entry['leagues']['h2h'])):
     league_id = get_data_entry['leagues']['h2h'][i]['id']
     league_name = get_data_entry['leagues']['h2h'][i]['name']
     previous_rank = get_data_entry['leagues']['h2h'][i]['entry_last_rank']
+    current_rank = get_data_entry['leagues']['h2h'][i]['entry_rank']
+    rank_difference = get_data_entry['leagues']['h2h'][i]['entry_last_rank'] - current_rank
 
+    league_api = 'https://fantasy.premierleague.com/api/leagues-h2h/%s/standings/' % league_id
+    get_data_league = session.get(league_api).json()
+    league_leader = get_data_league['standings']['results'][0]['total']
+    point_differential = league_leader - get_data_league['standings']['results'][current_rank - 1]['total']
 
-    #Needs to be chnaged currently no ['entry_movement'] field.
-    #There seems to be an actual 'rank' filed so none of this is necessary
-    
-    #if get_data_entry['leagues']['h2h'][i]['entry_movement'] == "up":
-        #current_rank = previous_rank - get_data_entry['leagues']['h2h'][i]['entry_change']
-    #elif get_data_entry['leagues']['h2h'][i]['entry_movement'] == "down":
-        #current_rank = previous_rank + get_data_entry['leagues']['h2h'][i]['entry_change']
-    #else:
-        #current_rank = previous_rank
-    #rank_difference = previous_rank - current_rank
-
-    
-
-    league_api = 'https://fantasy.premierleague.com/api/leagues-h2h-standings/%s' % league_id
-    get_data_league = get_data_team = requests.get(league_api).json()
-
-    league_leader = get_data_league['standings']['results'][0]['points_total']
-
-    print ("%-39s %-10d %-10d %-11d %d" %
-        (league_name, previous_rank, current_rank, rank_difference, league_leader))
+    print ("%-39s %-10d %-10d %-11d %d (%d)" %
+        (league_name, previous_rank, current_rank, rank_difference, league_leader, point_differential))
 
 print ("\n\nClassic Leagues:\n")
 print (sp*40 + "Prev" + sp*7 + "Curr" + sp*7 + "Rank" + sp*8 + "Leader")
@@ -183,19 +151,14 @@ for i in range (len(get_data_entry['leagues']['classic'])):
     league_id = get_data_entry['leagues']['classic'][i]['id']
     league_name = get_data_entry['leagues']['classic'][i]['name']
     previous_rank = get_data_entry['leagues']['classic'][i]['entry_last_rank']
-    if get_data_entry['leagues']['classic'][i]['entry_movement'] == "up":
-        current_rank = previous_rank - get_data_entry['leagues']['classic'][i]['entry_change']
-    elif get_data_entry['leagues']['classic'][i]['entry_movement'] == "down":
-        current_rank = previous_rank + get_data_entry['leagues']['classic'][i]['entry_change']
-    else:
-        current_rank = previous_rank
+    current_rank = get_data_entry['leagues']['classic'][i]["entry_rank"]
     rank_difference = previous_rank - current_rank
 
-    league_api = 'https://fantasy.premierleague.com/api/leagues-classic-standings/%s' % league_id
-    get_data_league = get_data_team = requests.get(league_api).json()
+    league_api = 'https://fantasy.premierleague.com/api/leagues-classic/%s/standings/' % league_id
+    get_data_league = session.get(league_api).json()
 
     league_leader = get_data_league['standings']['results'][0]['total']
-    league_difference = get_data_entry['entry']['summary_overall_points'] - league_leader
+    league_difference = league_leader - get_data_entry['summary_overall_points']
 
     print ("%-39s %-10d %-10d %-11d %d (%d)" % (league_name, previous_rank, current_rank,
         rank_difference, league_leader, league_difference))
