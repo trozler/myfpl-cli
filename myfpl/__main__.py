d518f7de2b4f28c4237d9c8ae85238036fef236d
Tony Rosler <tony.rosler246@gmail.com>
2020-09-13 18:37:28 +0200

    * Integrated arg parses. * Integrated use of config file. * removed need for password auth. ...
    Now need to test that pip install works. And also update readme.
    However, core functionality is done.

---
+++ myfpl/__main__.py
@@ -0,0 +1,233 @@
+#!/usr/bin/env python3
+
+import requests
+import getpass
+import re
+import argparse
+import sys
+import json
+
+from .live import liveRunner
+from .gameweek import gwRunner
+from .team import teamRunner
+
+team_id = None
+
+
+def addCli():
+    parser = argparse.ArgumentParser(
+        description="The myfpl cli allows you to retrieve information about your fpl team.", prog="myfpl",
+        epilog="\n"
+    )
+    parser.add_argument("-g", "--gameweek", action="store_true", dest="gameweek",
+                        help="See how your current gameweek is going by viewing points and league positions, as updated by fpl.")
+    parser.add_argument("-t",
+                        "--team", action="store_true", dest="team", help="Plan for future gameweeks by viewing transfers made, chips avialable, currently selected team and more.")
+    parser.add_argument("-l",
+                        "--live", action="store_true", dest="live", help="Show your classical league standings in real time, find out who your competitors have captained and how many transfers they made.")
+
+    parser.add_argument("-c",
+                        "--clear", action="store_true", dest="clear", help="Remove your email and team id stored in config.json.")
+
+    args = parser.parse_args()
+    if len(sys.argv) <= 1:
+        parser.print_help()
+        sys.exit(1)
+
+    return args
+
+
+def handleTeamId():
+    global team_id
+    flag = False
+    with open('./myfpl/config.json', 'r+') as jf:
+        config_data = json.load(jf)
+
+        if config_data["team_id"] == "":  # Enter your team id
+            flag = True
+
+            while True:
+                print("Enter team ID: ", end='')
+                temp = input()
+                try:
+                    temp = int(temp)
+                    if temp <= 0:
+                        print("please enter a valid team id.")
+                        continue
+                    team_id = temp
+                    config_data["team_id"] = team_id
+                    break
+                except:
+                    print("please enter a valid team id.")
+
+        else:
+            team_id = int(config_data["team_id"])
+
+        if flag:
+            jf.seek(0, 0)
+            json.dump(config_data, jf, ensure_ascii=False, indent=4)
+
+    return flag
+
+
+def handleLogin(credentials):
+    global team_id
+    flag = False
+    with open('./myfpl/config.json', 'r+') as jf:
+        config_data = json.load(jf)
+
+        if config_data["team_id"] == "":  # Enter your team id
+            flag = True
+
+            while True:
+                print("Enter team ID: ", end='')
+                temp = input()
+                try:
+                    temp = int(temp)
+                    if temp <= 0:
+                        print("please enter a valid team id.")
+                        continue
+                    team_id = temp
+                    config_data["team_id"] = team_id
+                    break
+                except:
+                    print("please enter a valid team id.")
+
+        else:
+            team_id = int(config_data["team_id"])
+
+        if config_data["email"] == "":  # Handle email
+            flag = True
+
+            while True:
+                print("Enter email: ", end='')
+                email = input()
+
+                credentials['login'] = email
+                config_data["email"] = email
+                break
+
+        else:
+            credentials['login'] = config_data["email"]
+
+        if flag:
+            jf.seek(0, 0)
+            json.dump(config_data, jf, ensure_ascii=False, indent=4)
+
+    credentials['password'] = getpass.getpass()
+
+    return flag
+
+
+def clearConfig():
+    with open('./myfpl/config.json', 'w') as jf:
+        config_data = {
+            "email": "",
+            "team_id": ""
+        }
+        json.dump(config_data, jf, ensure_ascii=False, indent=4)
+
+
+def main():
+    global team_id  # 27857
+
+    credentials = {'password': None,
+                   'login': None,
+                   'redirect_uri': 'https://fantasy.premierleague.com/a/login',
+                   'app': 'plfpl-web'
+                   }
+
+    args = addCli()
+    session = requests.session()
+
+    # Api fetched for all scripts.
+    bootstrap_url = 'https://fantasy.premierleague.com/api/bootstrap-static/'
+    get_data_bootstrap = session.get(bootstrap_url).json()
+
+    # Shoudl not ask for password.
+    if args.gameweek:
+
+        handleTeamId()  # Populate team_id
+
+        # Entry section. No login needed.
+        entry_api = 'https://fantasy.premierleague.com/api/entry/%s/' % team_id
+        get_data_entry = session.get(entry_api).json()
+
+        # Positive integer provided is too large clear data.
+        if len(get_data_entry) == 1:
+            print("Bad team id, please try again.")
+            clearConfig()
+            sys.exit(1)
+
+        if get_data_entry["current_event"] == None:
+            print(
+                "Fpl api/entry is not available currently, please try again when season returns.")
+            sys.exit(1)
+
+        # gw_Team API will always work no login needed.
+        gw_team_api = 'https://fantasy.premierleague.com/api/entry/%s/event/%s/picks/' % (
+            team_id, get_data_entry["current_event"])
+        get_gw_team = session.get(gw_team_api).json()
+
+        gwRunner(session, get_gw_team, get_data_bootstrap,
+                 get_data_entry)
+
+    # Should not ask for password.
+    elif args.live:
+
+        handleTeamId()  # Populate team_id
+
+        entry_api = 'https://fantasy.premierleague.com/api/entry/%s/' % team_id
+        get_data_entry = session.get(entry_api).json()
+
+        if len(get_data_entry) == 1:
+            print("Bad team id, please try again.")
+            clearConfig()
+            sys.exit(1)
+
+        if get_data_entry["current_event"] == None:
+            print(
+                "Premier League not in season, please come when season returns.")
+            sys.exit(1)
+
+        liveRunner(session, get_data_entry, get_data_bootstrap)
+
+    # Password needed
+    elif args.team:
+        curr_gw = None
+        for k in range(len(get_data_bootstrap["events"])):
+            if get_data_bootstrap["events"][k]["is_current"] == True:
+                curr_gw = k + 1
+
+        # Handle errors
+        if (curr_gw) == None:
+            print("Premier League not in season, please come when season returns.")
+            sys.exit(1)
+
+        handleLogin(credentials)
+
+        session.post('https://users.premierleague.com/accounts/login/',
+                     data=credentials)
+        credentials['login'] = None
+        credentials['password'] = None
+        team_api = 'https://fantasy.premierleague.com/api/my-team/%s/' % (
+            team_id)
+
+        get_data_team = session.get(team_api).json()
+
+        if len(get_data_team) == 1:  # If bad passowrd, emial or team_id. Always 200 response,
+            print("Wrong login information, please try again.")
+            clearConfig()
+            sys.exit(1)
+
+        teamRunner(session, team_id, get_data_team,
+                   get_data_bootstrap, curr_gw)
+
+    # TODO:Handle errors in all scripts.
+    if args.clear:
+        clearConfig()
+
+
+# Code will only run of this is main entry point to our package.
+if __name__ == "__main__":
+    main()
--- myfpl/live.py
+++ myfpl/live.py
@@ -1,105 +1,67 @@
 
-import requests
-import traceback
-import re
-import getpass
 from collections import OrderedDict
+from .gameweek import printGwTeam
 
-'''
-live leagues aims to fill the gap caused by FPL being slow to update leagues.
-It provides this service:
-- Real time league positions (net of transfers), before fpl officially updates tables.
-
-NOTE/
-- The program is computationally intensive, as such we will only output the first 200 entries of any league and users will be prompted to process more. 
-
-- FPL updates their tables at the end of each gameday, so several times per game week. Thus rank changes are always relative to the latest updated fpl table. 
-'''
-
-credentials = {'password': None,
-               'login': None,
-               'redirect_uri': 'https://fantasy.premierleague.com/a/login',
-               'app': 'plfpl-web'
-               }
-
-team_id = None
-
-# Only ask for validation if team_id, email and password havn't been set
-if team_id == None or credentials['login'] == None:
-    while True:
-        print("Enter team ID: ", end='')
-        team_id = input()
-        try:
-            int(team_id)
-        except:
-            print("Your game id is an all integer code")
-            continue
-
-        print("Email: ", end='')
-        email = input()
-        match = re.match(
-            '^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', email)
-        if match == None:
-            print('Bad email please re-enter')
-            continue
-        credentials['login'] = email
-
-        print("Password: ", end='')
-        credentials['password'] = getpass.getpass()
-        break
-
-# getpass method stops password echoing in terminal, menaing it remains hidden.
-else:
-    print("Password: ", end='')
-    credentials['password'] = getpass.getpass()
-
-# Establish session
-session = requests.session()
-session.post('https://users.premierleague.com/accounts/login/',
-             data=credentials)
-
-# Entry section
-entry_api = 'https://fantasy.premierleague.com/api/entry/%s/' % team_id
-get_data_entry = session.get(entry_api).json()
-
-# Bootstrap API
-bootstrap_url = 'https://fantasy.premierleague.com/api/bootstrap-static/'
-get_data_bootstrap = requests.get(bootstrap_url).json()
-
+# Globals
 league_map = OrderedDict()
 player_map = OrderedDict()
-
 sp = ' '
 
-print("\n\nClassic Leagues:\n")
-print('{0: >44}'.format('Prev'), '{0:>10}'.format('Curr'),
-      '{0: >10}'.format('Rank'), '{0: >12}'.format('Leader'))
-print("League Name", '{0:>32}'.format('Rank'), '{0: >10}'.format(
-    'Rank'), '{0: >10}'.format('Diff'), '{0: >13}'.format("Points\n"))
 
-for i in range(len(get_data_entry['leagues']['classic'])):
-    league_id = get_data_entry['leagues']['classic'][i]['id']
-    league_name = get_data_entry['leagues']['classic'][i]['name']
-    previous_rank = get_data_entry['leagues']['classic'][i]['entry_last_rank']
-    current_rank = get_data_entry['leagues']['classic'][i]["entry_rank"]
-    rank_difference = previous_rank - current_rank
+def liveRunner(session, get_data_entry, get_data_bootstrap):
+    global league_map, player_map
+    print("\nClassic Leagues:\n")
+    print('{0: >44}'.format('Prev'), '{0:>10}'.format('Curr'),
+          '{0: >10}'.format('Rank'), '{0: >12}'.format('Leader'))
+    print("League Name", '{0:>32}'.format('Rank'), '{0: >10}'.format(
+        'Rank'), '{0: >10}'.format('Diff'), '{0: >13}'.format("Points\n"))
 
-    league_api = 'https://fantasy.premierleague.com/api/leagues-classic/%s/standings/' % league_id
-    get_data_league = session.get(league_api).json()
+    for i in range(len(get_data_entry['leagues']['classic'])):
+        league_id = get_data_entry['leagues']['classic'][i]['id']
+        league_name = get_data_entry['leagues']['classic'][i]['name']
+        previous_rank = get_data_entry['leagues']['classic'][i]['entry_last_rank']
+        current_rank = get_data_entry['leagues']['classic'][i]["entry_rank"]
+        rank_difference = previous_rank - current_rank
 
-    league_leader = get_data_league['standings']['results'][0]['total']
-    league_difference = league_leader - \
-        get_data_entry['summary_overall_points']
+        league_api = 'https://fantasy.premierleague.com/api/leagues-classic/%s/standings/' % league_id
+        get_data_league = session.get(league_api).json()
 
-    # Create league hashmap. Value is a tuple of name and league id.
-    league_map[i] = (league_name, league_id)
+        if len(get_data_league['standings']['results']) == 0:
+            print(
+                "Fpl end-point api/leagues-classic, is not working, please try again later.")
+            return
 
-    print("(%d) %-35s %-10d %-10d %-10d %d (%d)" % (i, league_name, previous_rank, current_rank,
-                                                    rank_difference, league_leader, league_difference))
+        league_leader = get_data_league['standings']['results'][0]['total']
+        league_difference = league_leader - \
+            get_data_entry['summary_overall_points']
+
+        # Create league hashmap. Value is a tuple of name and league id.
+        league_map[i] = (league_name, league_id)
+
+        print("(%d) %-35s %-10d %-10d %-10d %d (%d)" % (i, league_name, previous_rank, current_rank,
+                                                        rank_difference, league_leader, league_difference))
+
+    while True:
+        print(
+            "\nEnter the ID (left of name) of the league you want live standings for or q to exit:")
+        try:
+            ID = input()
+            if ID == 'q':
+                break
+            ID = int(ID)
+            if ID not in league_map:
+                continue
+            else:
+                process_league(ID, session, get_data_entry, get_data_bootstrap)
+
+        except:
+            continue
+
+    print()
 
 
-def process_league(ID):
-
+def process_league(ID, session, get_data_entry, get_data_bootstrap):
+    global league_map, player_map
     # Orderd dict for sorting efficiency
     user_map = OrderedDict()
     page_num = 1
@@ -121,9 +83,13 @@ def process_league(ID):
                 team_id, gameweek)
             get_gw_team = session.get(gw_team_api).json()
             # get old gw points - costly try and find improvement in future
-            gw_team_old_api = 'https://fantasy.premierleague.com/api/entry/%s/event/%s/picks/' % (
-                team_id, gameweek - 1 if gameweek > 0 else gameweek)
-            get_gw_old_team = session.get(gw_team_old_api).json()
+            if gameweek > 1:
+                gw_team_old_api = 'https://fantasy.premierleague.com/api/entry/%s/event/%s/picks/' % (
+                    team_id, gameweek - 1)
+                get_gw_old_team = session.get(gw_team_old_api).json()
+                gw_old_points = get_gw_old_team["entry_history"]["total_points"]
+            else:
+                gw_old_points = 0
 
             speed_gw_points = 0
             # Captain points(already doubled), Name, triple (boolean)
@@ -178,19 +144,21 @@ def process_league(ID):
                         captain_id_name[0], captain_id_name[1] = 0, player_map[id][1]
 
             # Have Team name + person name of as key. Value is a tuple:
-            # (gw-1 points + gw points - hits, gw points - hits, previous rank, hits)
-            user_map[user_id] = (speed_gw_points + get_gw_old_team["entry_history"]["total_points"] - get_gw_team["entry_history"]["event_transfers_cost"], speed_gw_points -
-                                 get_gw_team["entry_history"]["event_transfers_cost"], get_data_league["standings"]["results"][i]["rank"], -get_gw_team["entry_history"]["event_transfers_cost"], captain_id_name)
+            # (gw-1 points + gw points - hits, gw points - hits, previous rank, hits, gw team)
+            user_map[user_id] = (speed_gw_points + gw_old_points - get_gw_team["entry_history"]["event_transfers_cost"], speed_gw_points - get_gw_team["entry_history"]["event_transfers_cost"],
+                                 get_data_league["standings"]["results"][i]["rank"],
+                                 -get_gw_team["entry_history"]["event_transfers_cost"],
+                                 captain_id_name, get_gw_team)
 
-        # Only print first 200 entries. Then ask if wnat to keep going
+        # Only print first 200 entries. Then ask if want to keep going
         if page_num == limit_page or len(get_data_league['standings']['results']) == 0:
             # Ask if keep going, as there exsits a next page.
             if len(get_data_league['standings']['results']) != 0 and page_num == limit_page:
                 print(
-                    '\nEnter any key to process another 200 players; or enter -1 to see output: ')
+                    "\nEnter 'go' to see output or enter anything else to process another 200 players:")
                 try:
-                    temp = int(input())
-                    if temp == -1:
+                    temp = input()
+                    if temp == 'go':
                         break
                     else:
                         page_num += 1
@@ -235,17 +203,13 @@ def process_league(ID):
         print("%-44s %-9d %-9d %-8d %-27s %-7d %d (%d)" % (user_list[i][0], i + 1, user_list[i][1][2], user_list[i][1][2] - (i + 1), ((user_list[i][1][4][1] + ' ('+str(
             user_list[i][1][4][0]) + ')' + '(TC)') if user_list[i][1][4][2] else user_list[i][1][4][1] + ' ('+str(user_list[i][1][4][0]) + ')'), user_list[i][1][3], user_list[i][1][0], user_list[i][1][1]))
 
-
-while True:
-    print("\nEnter the ID (left of name) of the league you want live standings or -1 to exit:")
-    try:
-        ID = int(input())
-        if ID == -1:
+    while True:
+        print("\nView a players team by entering their current rank:")
+        try:
+            ID = int(input())
+            if 1 <= ID <= len(user_list):
+                print("\n" + user_list[ID - 1][0])
+                printGwTeam(user_list[ID - 1][1][5],
+                            get_data_bootstrap, get_data_entry)
+        except:
             break
-        elif ID not in league_map:
-            continue
-        else:
-            process_league(ID)
-            print()
-    except:
-        continue
